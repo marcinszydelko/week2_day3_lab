4a358995e8387180e62b1f751eb60f789cafc686
Marcin <marcinszydelko@yahoo.com>
2019-10-23 10:48:03 +0100

    adds remove drink function

--- pub.rb
+++ pub.rb
@@ -1,18 +1,20 @@
 class Pub
 
- attr_reader :name, :till, :drinks
+   attr_reader :name, :till, :drinks
 
- def initialize(name, till)
-   @name = name
-   @till = till
-   @drinks = []
- end
+   def initialize(name, till, drinks)
+     @name = name
+     @till = till
+     @drinks = drinks
+   end
 
- def increase_till(amount)
-  @till += amount
- end
+   def increase_till(amount)
+    @till += amount
+   end
 
-
+   def remove_drink(drink)
+     @drinks.delete(drink)
+   end
 
 
 
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -1,18 +1,23 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../pub')
+require_relative('../drink')
 
 class TestPub < MiniTest::Test
 
 
   def setup
-    @pub1 = Pub.new("Ruby Inn", 1000)
+    @drink1 = Drink.new("Tenants", 3, 5)
+    @drink2 = Drink.new("Best", 4, 6)
+    # @stock = [@drink1, @drink2]
+    @pub1 = Pub.new("Ruby Inn", 1000,[@drink1, @drink2])
+
   end
 
   def test_pub_attributes
     assert_equal("Ruby Inn", @pub1.name)
     assert_equal(1000, @pub1.till)
-    assert_equal([], @pub1.drinks)
+    assert_equal([@drink1, @drink2], @pub1.drinks)
   end
 
   def test_increase_till
@@ -20,7 +25,10 @@ class TestPub < MiniTest::Test
     assert_equal(1005, @pub1.till)
   end
 
-
+  def test_remove_drink
+    @pub1.remove_drink(@drink1)
+    assert_equal([@drink2], @pub1.drinks)
+  end
 
 
 
